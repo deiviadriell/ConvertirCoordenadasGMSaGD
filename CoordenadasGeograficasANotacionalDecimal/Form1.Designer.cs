97e01aa67d1a4326b60c855b91177bc88f25bea8
David <David@DEIVI>
2018-12-26 14:57:03 -0500

    Todos los archivos

---
+++ CoordenadasGeograficasANotacionalDecimal/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace CoordenadasGeograficasANotacionalDecimal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRutaArchivo = new System.Windows.Forms.TextBox();
+            this.btnCargarExcel = new System.Windows.Forms.Button();
+            this.grpExcelCargado = new System.Windows.Forms.GroupBox();
+            this.dgvExcelSeleccionado = new System.Windows.Forms.DataGridView();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.opnSeleccionArchivo = new System.Windows.Forms.OpenFileDialog();
+            this.grpColumnas = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboColumnaLongitud = new System.Windows.Forms.ComboBox();
+            this.cboColumnaLatitud = new System.Windows.Forms.ComboBox();
+            this.grpResultado = new System.Windows.Forms.GroupBox();
+            this.dgvResultado = new System.Windows.Forms.DataGridView();
+            this.grpExcelCargado.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvExcelSeleccionado)).BeginInit();
+            this.grpColumnas.SuspendLayout();
+            this.grpResultado.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtRutaArchivo
+            // 
+            this.txtRutaArchivo.Location = new System.Drawing.Point(12, 12);
+            this.txtRutaArchivo.Name = "txtRutaArchivo";
+            this.txtRutaArchivo.Size = new System.Drawing.Size(378, 20);
+            this.txtRutaArchivo.TabIndex = 5;
+            // 
+            // btnCargarExcel
+            // 
+            this.btnCargarExcel.Location = new System.Drawing.Point(401, 10);
+            this.btnCargarExcel.Name = "btnCargarExcel";
+            this.btnCargarExcel.Size = new System.Drawing.Size(75, 23);
+            this.btnCargarExcel.TabIndex = 4;
+            this.btnCargarExcel.Text = "Cargar Excel";
+            this.btnCargarExcel.UseVisualStyleBackColor = true;
+            this.btnCargarExcel.Click += new System.EventHandler(this.btnCargarExcel_Click);
+            // 
+            // grpExcelCargado
+            // 
+            this.grpExcelCargado.Controls.Add(this.dgvExcelSeleccionado);
+            this.grpExcelCargado.Location = new System.Drawing.Point(12, 38);
+            this.grpExcelCargado.Name = "grpExcelCargado";
+            this.grpExcelCargado.Size = new System.Drawing.Size(509, 585);
+            this.grpExcelCargado.TabIndex = 6;
+            this.grpExcelCargado.TabStop = false;
+            this.grpExcelCargado.Text = "Archivo de Excel ";
+            // 
+            // dgvExcelSeleccionado
+            // 
+            this.dgvExcelSeleccionado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvExcelSeleccionado.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvExcelSeleccionado.Location = new System.Drawing.Point(3, 16);
+            this.dgvExcelSeleccionado.Name = "dgvExcelSeleccionado";
+            this.dgvExcelSeleccionado.Size = new System.Drawing.Size(503, 566);
+            this.dgvExcelSeleccionado.TabIndex = 0;
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Location = new System.Drawing.Point(728, 597);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(75, 23);
+            this.btnConvertir.TabIndex = 7;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnSiguiente_Click);
+            // 
+            // opnSeleccionArchivo
+            // 
+            this.opnSeleccionArchivo.FileName = "openFileDialog1";
+            // 
+            // grpColumnas
+            // 
+            this.grpColumnas.Controls.Add(this.label2);
+            this.grpColumnas.Controls.Add(this.label1);
+            this.grpColumnas.Controls.Add(this.cboColumnaLongitud);
+            this.grpColumnas.Controls.Add(this.cboColumnaLatitud);
+            this.grpColumnas.Location = new System.Drawing.Point(527, 38);
+            this.grpColumnas.Name = "grpColumnas";
+            this.grpColumnas.Size = new System.Drawing.Size(276, 553);
+            this.grpColumnas.TabIndex = 8;
+            this.grpColumnas.TabStop = false;
+            this.grpColumnas.Text = "Columnas a seleccionar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Columna Longitud";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Columna Latitud";
+            // 
+            // cboColumnaLongitud
+            // 
+            this.cboColumnaLongitud.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboColumnaLongitud.FormattingEnabled = true;
+            this.cboColumnaLongitud.Location = new System.Drawing.Point(110, 46);
+            this.cboColumnaLongitud.Name = "cboColumnaLongitud";
+            this.cboColumnaLongitud.Size = new System.Drawing.Size(160, 21);
+            this.cboColumnaLongitud.TabIndex = 1;
+            // 
+            // cboColumnaLatitud
+            // 
+            this.cboColumnaLatitud.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboColumnaLatitud.FormattingEnabled = true;
+            this.cboColumnaLatitud.Location = new System.Drawing.Point(110, 19);
+            this.cboColumnaLatitud.Name = "cboColumnaLatitud";
+            this.cboColumnaLatitud.Size = new System.Drawing.Size(160, 21);
+            this.cboColumnaLatitud.TabIndex = 0;
+            // 
+            // grpResultado
+            // 
+            this.grpResultado.Controls.Add(this.dgvResultado);
+            this.grpResultado.Location = new System.Drawing.Point(819, 38);
+            this.grpResultado.Name = "grpResultado";
+            this.grpResultado.Size = new System.Drawing.Size(598, 585);
+            this.grpResultado.TabIndex = 9;
+            this.grpResultado.TabStop = false;
+            this.grpResultado.Text = "Resultado";
+            // 
+            // dgvResultado
+            // 
+            this.dgvResultado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResultado.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvResultado.Location = new System.Drawing.Point(3, 16);
+            this.dgvResultado.Name = "dgvResultado";
+            this.dgvResultado.Size = new System.Drawing.Size(592, 566);
+            this.dgvResultado.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1427, 624);
+            this.Controls.Add(this.grpResultado);
+            this.Controls.Add(this.grpColumnas);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.grpExcelCargado);
+            this.Controls.Add(this.txtRutaArchivo);
+            this.Controls.Add(this.btnCargarExcel);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Convertir coordenadas geográficas a notación decimal";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpExcelCargado.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvExcelSeleccionado)).EndInit();
+            this.grpColumnas.ResumeLayout(false);
+            this.grpColumnas.PerformLayout();
+            this.grpResultado.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResultado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRutaArchivo;
+        private System.Windows.Forms.Button btnCargarExcel;
+        private System.Windows.Forms.GroupBox grpExcelCargado;
+        private System.Windows.Forms.DataGridView dgvExcelSeleccionado;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.OpenFileDialog opnSeleccionArchivo;
+        private System.Windows.Forms.GroupBox grpColumnas;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboColumnaLongitud;
+        private System.Windows.Forms.ComboBox cboColumnaLatitud;
+        private System.Windows.Forms.GroupBox grpResultado;
+        private System.Windows.Forms.DataGridView dgvResultado;
+    }
+}
+
---
+++ CoordenadasGeograficasANotacionalDecimal/Form1.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using ExcelDataReader;
+using ExcelDataReader.Core;
+using ExcelDataReader.Log;
+
+namespace CoordenadasGeograficasANotacionalDecimal
+{
+    public partial class Form1 : Form
+    {
+        public DataTable firstTable = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            this.Size = new Size(544, 663);
+        }
+
+        private void btnCargarExcel_Click(object sender, EventArgs e)
+        {
+            resetCarga();
+            string path = "";
+            int celda = -1;
+            opnSeleccionArchivo.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+            opnSeleccionArchivo.InitialDirectory = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (opnSeleccionArchivo.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+
+                path = opnSeleccionArchivo.FileName;
+                txtRutaArchivo.Text = path;
+                
+                
+                using (var stream = File.Open(path, FileMode.Open, FileAccess.Read))
+                {
+                    using (var reader = ExcelReaderFactory.CreateOpenXmlReader(stream))
+                    {
+                        
+                        DataSet dts = reader.AsDataSet();
+                        //obtengo el resultado como un dataSet
+                        firstTable = dts.Tables[0];
+                        stream.Close();
+                        if (firstTable.Rows.Count > 1)
+                        {
+
+                            //sin 
+                            cambiarDimensionesPantalla(825, 663);
+                            
+                            int y = 80;
+                            cboColumnaLatitud.Items.Add("Seleccione un valor");
+                            cboColumnaLongitud.Items.Add("Seleccione un valor");
+
+
+                            foreach (DataColumn column in firstTable.Columns)
+                            {
+                                string cName = firstTable.Rows[0][column.ColumnName].ToString();
+                                if (!firstTable.Columns.Contains(cName) && cName != "")
+                                {
+                                    column.ColumnName = cName;
+
+                                    cboColumnaLatitud.Items.Add(cName);
+                                    cboColumnaLongitud.Items.Add(cName);
+                                    CheckBox chk = new CheckBox();
+                                    chk.Text = cName;
+                                    chk.Location = new Point(9, y);
+                                    y += 23;
+                                    grpColumnas.Controls.Add(chk);
+                                }
+
+                            }
+                            cboColumnaLatitud.SelectedIndex = 0;
+                            cboColumnaLongitud.SelectedIndex = 0;
+                            firstTable.Rows[0].Delete();
+                            dgvExcelSeleccionado.DataSource = firstTable;
+                            reader.Close();
+
+
+                        }
+                        else
+                        { }
+                        
+                    }
+
+                }
+            }
+        }
+
+
+        private void cambiarDimensionesPantalla(int x, int y)
+        {
+            this.Size = new Size(x, y);
+        }
+        public void transformations(string valor, ref string grado, ref string minuto, ref string segundo, ref string direccion, ref string observacion, ref string decimales, int lat)
+        {
+            try
+            {
+
+                grado = valor.Substring(0, valor.IndexOf('°'));
+                valor = valor.Substring((grado.Length), (valor.Length - grado.Length));
+                valor = valor.Remove(0, 1);
+                minuto = valor.Substring(0, valor.IndexOf('\''));                
+                valor = valor.Substring((minuto.Length), (valor.Length - minuto.Length));
+                valor = valor.Remove(0, 1);
+                if (valor.IndexOf('\"') == -1)
+                {
+                    segundo = "0";
+                }
+                else
+                {
+                    segundo = valor.Substring(0, valor.IndexOf('\"'));
+                    valor = valor.Substring((segundo.Length), (valor.Length - segundo.Length));
+                }
+                direccion = valor;
+                if (direccion.Length > 1)
+                    direccion = direccion.Remove(0, 1);
+                observacion = "correcto";
+                if (lat == 1)
+                    decimales = convertirCoordenadasADecimales(grado, minuto, segundo, direccion,"latitud");
+                else
+                    decimales = convertirCoordenadasADecimales(grado, minuto, segundo, direccion,"longitud");
+                
+            }
+            catch (Exception ex)
+            {
+                observacion = "error";
+            }
+        }      
+        public string convertirCoordenadasADecimales(string grados, string minutos, string segundos, string direccion,string conversion)
+        {
+            if (conversion == "latitud")
+            {
+                int latitude_sign = 1;
+                if (direccion.ToLower() == "s")
+                    latitude_sign = -1;
+                double latitude_sec, latitude_min, aux = 1000000.0, absdlat, absmlat, absslat;
+                latitude_sec = Math.Abs(Math.Round(Convert.ToDouble(segundos) * aux) / 1000000);
+                latitude_min = Math.Abs(Math.Round(Convert.ToDouble(minutos) * aux) / 1000000);
+
+                // Calculations
+
+                absdlat = Math.Abs(Math.Round(Convert.ToInt32(grados) * 1000000.0));
+                absmlat = Math.Abs(Math.Round(latitude_min * 1000000.0));
+                absslat = Math.Abs(Math.Round(latitude_sec * 1000000.0));
+                // Final calculation
+
+                string latitude = "";
+                latitude = (Math.Round(absdlat + (absmlat / 60) + (absslat / 3600.0)) * latitude_sign / 1000000).ToString();
+                //longitude = Math.round(absdlon + (absmlon / 60) + (absslon / 3600)) * longitude_sign / 1000000;
+
+
+                return latitude;
+            }
+            else
+            {
+
+                int longitude_sign = 1;
+                if (direccion.ToLower() == "w")
+                    longitude_sign = -1;
+                double longitude_min, longitude_sec, aux = 1000000.0, absdlon, absmlon, absslon;
+                /*latitude_sec = Math.Abs(Math.Round(Convert.ToInt32(segundos) * aux) / 1000000);
+                latitude_min = Math.Abs(Math.Round(Convert.ToUInt32(minutos) * aux) / 1000000);*/
+
+                longitude_min = Math.Abs(Math.Round(Convert.ToDouble(minutos) * 1000000.0) / 1000000);
+                longitude_sec = Math.Abs(Math.Round(Convert.ToDouble(segundos) * 1000000.0) / 1000000);
+
+                // Calculations
+
+                /*absdlat = Math.Abs(Math.Round(Convert.ToInt32(grados) * 1000000.0));
+                absmlat = Math.Abs(Math.Round(latitude_min * 1000000.0));
+                absslat = Math.Abs(Math.Round(latitude_sec * 1000000.0));*/
+
+                absdlon = Math.Abs(Math.Round(Convert.ToInt32(grados) * 1000000.0));
+                absmlon = Math.Abs(Math.Round(longitude_min * 1000000));
+                absslon = Math.Abs(Math.Round(longitude_sec * 1000000.0));
+                // Final calculation
+
+                string longitud = "";
+                longitud = (Math.Round(absdlon + (absmlon / 60) + (absslon / 3600)) * longitude_sign / 1000000).ToString();
+
+
+                return longitud;
+
+            }
+        }        
+        private void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            if (cboColumnaLatitud.SelectedIndex != 0 && cboColumnaLongitud.SelectedIndex != 0)
+            {
+                if (cboColumnaLatitud.SelectedIndex != cboColumnaLongitud.SelectedIndex)
+                {
+                    //List<string> nombresColumnasSeleccionadas = new List<string>();
+                    DataTable tbResults = new DataTable();
+                    foreach (Control controles in grpColumnas.Controls)
+                    {
+                        if ((controles is CheckBox) && ((CheckBox)controles).Checked)
+                            tbResults.Columns.Add(controles.Text);                                                    
+                    }
+                    tbResults.Columns.Add("grados latitud");
+                    tbResults.Columns.Add("minutos latitud");
+                    tbResults.Columns.Add("segundos latitud");
+                    tbResults.Columns.Add("direccion latitud");
+                    tbResults.Columns.Add("Grados Decimales Latitud");
+                    tbResults.Columns.Add("observacion Latitud");
+
+                    tbResults.Columns.Add("grados longitud");
+                    tbResults.Columns.Add("minutos longitud");
+                    tbResults.Columns.Add("segundos longitud");
+                    tbResults.Columns.Add("direccion longitud");
+                    tbResults.Columns.Add("Grados Decimales Longitud");
+                    tbResults.Columns.Add("observacion Longitud");
+                    for (int i = 1; i < firstTable.Rows.Count; i++)
+                    {
+                        DataRow row = tbResults.NewRow();
+                        foreach (Control controles in grpColumnas.Controls)
+                        {
+                            if ((controles is CheckBox) && ((CheckBox) controles).Checked)                                
+                                    row[controles.Text] = firstTable.Rows[i][controles.Text].ToString();
+                        }
+                        
+                        string grado = "", minuto = "", segundo = "", direccion = "", observacion = "", decimales = "";
+                        transformations(firstTable.Rows[i][cboColumnaLatitud.SelectedIndex-1].ToString(), ref grado, ref minuto, ref segundo, ref direccion, ref observacion, ref decimales,1);
+                        row["grados latitud"] = grado;
+                        row["minutos latitud"] = minuto;
+                        row["segundos latitud"] = segundo;
+                        row["direccion latitud"] = direccion;
+                        row["Grados Decimales Latitud"] = decimales;
+                        row["observacion Latitud"] = observacion;
+                        grado = ""; minuto = ""; segundo = ""; direccion = ""; observacion = ""; decimales = "";
+                        transformations(firstTable.Rows[i][cboColumnaLongitud.SelectedIndex-1].ToString(), ref grado, ref minuto, ref segundo, ref direccion, ref observacion, ref decimales,0);
+
+                        row["grados longitud"]=grado;
+                        row["minutos longitud"]=minuto;
+                        row["segundos longitud"]=segundo;
+                        row["direccion longitud"] = direccion;
+                        row["Grados Decimales Longitud"]=decimales;
+                        row["observacion Longitud"]=observacion;
+                        tbResults.Rows.Add(row);
+                    }
+                    dgvResultado.DataSource = tbResults;
+                    cambiarDimensionesPantalla(1440, 663);
+                    btnExportarExcelResultado.Visible = true;
+                }
+                else
+                { MessageBox.Show("Debe seleccionar columnas diferentes para realizar la conversión de las coordenadas", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+
+            }
+            else
+            { MessageBox.Show("Debe de Seleccionar la columna que contiene la latitud y la longitud para poder realizar la conversión", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+        }
+        private void resetCarga()
+        {
+            dgvExcelSeleccionado.DataSource = null;
+            dgvResultado.DataSource = null;
+            cboColumnaLatitud.Items.Clear();
+            cboColumnaLongitud.Items.Clear();
+            txtRutaArchivo.Text = "";
+
+           
+        }
+
+        
+    }
+}
